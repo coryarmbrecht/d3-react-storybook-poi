e662ba162a3b9cc9d323e768cb75c96b70044c5b
Cory Armbrecht <cory.armbrecht@gmail.com>
2018-01-02 22:53:06 -0800

    Added moment, renders svg rectangles and text, and an an x-axis. Now I need to make all these measurements responsive.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as d3 from 'd3'
+import moment from 'moment'
 
 console.log('index.js')
 
@@ -9,20 +10,18 @@ const sum = (a,b) => {
 }
 
 
-
 const tasks = 
     [
-        {task:'Start project', date: new Date(2018, 0, 1)},
-        {task:'Get to the first quarter', date: new Date(2018, 1, 5)},
-        {task:'Halfway', date: new Date(2018, 2, 1)},
-        {task:'Finish the project', date: new Date(2018, 3, 2)}
+        {task:'Start project', goalDate: new Date(2018, 0, 1), startDate: '', createdDate: '', progress:.2},
+        {task:'Get to the first quarter', goalDate: new Date(2018, 0, 28), startDate: '', createdDate: '', progress:.15},
+        {task:'Halfway', goalDate: new Date(2018, 2, 1), startDate: '', createdDate: '', progress:.07},
+        {task:'Finish the project', goalDate: new Date(2018, 3, 2), startDate: '', createdDate: '', progress:.04}
     ]
 
 const startDate = new Date(2018,0,1)
 const goalDate = new Date(2018,3,6)
 const currentDate = new Date()
-const finished = false
-
+const progress = 0
 
 export const timeScale = d3.scaleTime()
     .domain([startDate, goalDate])
@@ -34,25 +33,59 @@ d3.select('#app')
     .append('div')
         .html(d3.version)
 */
-const format = d3.format(".2")
 
+const xScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([300,0])
+
+const xAxis = d3.axisBottom(xScale)
+
+
+const format = d3.format(".2")
 
 d3.select('#app')
     .append('div')
-        .html(format(timeScale(currentDate)) + ' %')
-
+        .html('Time Used: ' + format(timeScale(currentDate)) + '%')
 
 const d3Tasks = d3.select('#app')
-    .selectAll('p')
-    .data(tasks)
+    .append('svg').call(xAxis)
+        .style('overflow', 'visible')
+        .style('width','100%')
+        .style('height', '100%')
+        .append('g')
+    .selectAll('g')
+    
+        .data(tasks)
+        .enter()
+        .append('g')
+        .attr('transform', d => 'translate('+ d.progress * 1000 +','+ d.progress * 100 +')')
 
-d3Tasks.enter()
-    .append('div')
-    .text((d)=>{
-        console.log('hhheeeeyyy')
-        return '' + d.task + ' ' + format(timeScale(d.date)) + '%'
+//d3Tasks
+
+d3Tasks
+    .append('rect')
+    .style('width', d => d.progress * 1000)
+    .style('height', d => d.progress * 1000)
+    .style('fill', 'pink')
+    
+d3Tasks
+    .append('text')
+        .text((d)=>{
+        console.log('d3Tasks.enter d = ', d )
+        return '' + d.task + ' start: ' + moment(startDate).format('MMMM Do YYYY, h:mm:ss a') + '  end: ' + moment(goalDate).format('MMMM Do YYYY, h:mm:ss a') + '   ' + format(timeScale(d.goalDate)) + '%'
     })
+    .attr('x', d => d.progress * 1000)
+    .attr('y', d => d.progress * 1000)
+    .style('fill','black')
+    //.style('stroke-width', '1px')
+    
 //console.log('sum(1,2) = ', sum(1,2))
+
+
+
+
+
+
 export default sum
 
 
